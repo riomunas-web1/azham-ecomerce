1a8c7f76c4112cee97ef39e31b22d9bbf37d4346
azhamcool <azhamcool@gmail.com>
2014-02-09 17:35:52 +0700

    session yang masih salah

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
-<?php
-session_start();
+    <?php
+    session_start();
+    include "koneksi.php";
+    error_reporting(0);
 
-if(!isset($_SESSION['username'])){
-    //jika session belum di set/register
-    die("Anda belum register kan session");
-}
+    if (empty($_SESSION['username']) AND empty($_SESSION['password'])){include "input_form_login.php";}
+    else{
+    ?>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <title>Login</title>
+    </head>
+    <body>
 
-//jika sudah register kita lanjut
-?>
-<h2>Selamat Datang</h2>
-
-<?php
-echo $_SESSION['username'];
-?>
-<html>
-<head>
-<title>Halaman Sukses Login</title>
-</head>
-<body>
-<center>
-
-<a href="logout.php"><b>Logout</a>
-</center>
-</body>
-</html>
+    <?php
+    $sql = mysql_query("SELECT * FROM register WHERE username = '$_SESSION[username]', '$_SESSION[password]'");
+    while ($data = mysql_fetch_array($sql))
+    {
+     $username = $data['username'];
+    }
+    ?>
+    <h2>Selamat Datang <?php echo "$username"; ?>!</h2>
+       </body>
+    </html>
+    <?php } ?>
