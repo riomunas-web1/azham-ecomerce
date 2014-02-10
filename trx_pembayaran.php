cefe764bd70e88b83f6ecab0577ee482a367e96b
riomunas <rio.munas@gmail.com>
2014-02-11 02:22:31 +0700

    commit

--- trx_pembayaran.php
+++ trx_pembayaran.php
@@ -1,27 +1,49 @@
 <?php
-
 require_once('Connections/koneksi.php');
 session_start();
-
-if (isset($_SESSION['keranjang'])) {
-    $total = 0;
-    foreach ($_SESSION['keranjang'] as $item) {
-        mysql_select_db($database_koneksi, $koneksi);
-        $query = sprintf(
-                "select * "
-                . "from barang_koleksi "
-                . "left join barang_jenis on barang_koleksi.barang_jenis = barang_jenis.sid "
-                . "where barang_koleksi.sid = '%s'", $item['sid']);
-        $result = mysql_query($query) or die(mysql_error());
-        $barang_koleksi = mysql_fetch_array($result);
-        
-        echo 'nama barang : '.$barang_koleksi['nama_barang'].'<br/>';
-        echo 'qty : '.(int) $item['qty'].'<br/>';
-        echo '@harga'.$barang_koleksi['harga'].'<br/>';
-        echo 'jumlah : Rp. '.number_format((int) $item['qty'] * $barang_koleksi['harga'], 2, ',', '.').'<br/>';
-        $total = $total + (int) $item['qty'] * $barang_koleksi['harga'];
-//        print_r($barang_koleksi);
-        echo '<hr/>';
+?>
+<table border="1">
+    <?php if (isset($_SESSION['keranjang'])) { ?>
+        <tr>
+            <td>Nama Barang</td>
+            <td colspan="2">@harga</td>
+            <td>Qty</td>
+            <td colspan="2">Jumlah</td>
+        </tr><?php
+        $total = 0;
+        foreach ($_SESSION['keranjang'] as $item) {
+            mysql_select_db($database_koneksi, $koneksi);
+            $query = sprintf(
+                    "select * "
+                    . "from barang_koleksi "
+                    . "left join barang_jenis on barang_koleksi.barang_jenis = barang_jenis.sid "
+                    . "where barang_koleksi.sid = '%s'", $item['sid']);
+            $result = mysql_query($query) or die(mysql_error());
+            $barang_koleksi = mysql_fetch_array($result);
+            ?>
+            <tr>
+                <td><?php echo $barang_koleksi['nama_barang'] ?></td>
+                <td>Rp. </td>
+                <td><?php echo number_format($barang_koleksi['harga'], 0, ',', '.') ?></td>
+                <td><?php echo (int) $item['qty'] ?></td>
+                <td>Rp. </td>
+                <td style="text-align: right"><?php echo number_format((int) $item['qty'] * $barang_koleksi['harga'], 0, ',', '.') ?></td>
+            </tr>
+            <?php
+//            echo 'nama barang : ' . $barang_koleksi['nama_barang'] . '<br/>';
+//            echo 'qty : ' . (int) $item['qty'] . '<br/>';
+//            echo '@harga' . $barang_koleksi['harga'] . '<br/>';
+//            echo 'jumlah : Rp. ' . number_format((int) $item['qty'] * $barang_koleksi['harga'], 2, ',', '.') . '<br/>';
+            $total = $total + (int) $item['qty'] * $barang_koleksi['harga'];
+//            echo '<hr/>';
+        }
+        ?>
+            <tr>
+                <td colspan="4" style="text-align: right">Total : </td>
+                <td >Rp. </td>
+                <td ><?php echo number_format($total, 0, ',', '.') ?></td>
+            </tr>
+        <?php
     }
-    echo 'total : Rp. '.number_format($total, 2, ',', '.');
-}
+    ?>
+</table>
