16b9a757cc720a0f53fe14c9e8a0d958442417c5
azhamcool <azhamcool@gmail.com>
2014-02-22 22:30:06 +0700

    commit

--- loginadmin.php
+++ loginadmin.php
@@ -38,8 +38,8 @@ if (isset($_SERVER['QUERY_STRING'])) {
 
 if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
   $insertSQL = sprintf("INSERT INTO `admin` (sid, userid, passid) VALUES (uuid(), 'user1'. 'rahasia')",
-                       GetSQLValueString($_POST['userid'], "text"),
-                       GetSQLValueString($_POST['passid'], "text"));
+                       GetSQLValueString($_POST['userid'], "int"),
+                       GetSQLValueString($_POST['passid'], "int"));
 
 
 
@@ -50,7 +50,7 @@ if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
 if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form2")) {
   $insertSQL = sprintf("INSERT INTO `admin` (userid, passid) VALUES (%s, %s)",
                        GetSQLValueString($_POST['userid'], "text"),
-                       GetSQLValueString($_POST['passid'], "text"));
+                       GetSQLValueString($_POST['passid'], "int"));
 if (empty($_POST['userid']) || empty($_POST['passid'])) {  
         echo '<script language="javascript">  
         alert("Data Harus Diisi Semua");  
@@ -59,7 +59,9 @@ if (empty($_POST['userid']) || empty($_POST['passid'])) {
          exit(); 
     }  
     else {  
-      
+      echo '<script language="javascript">alert("Data Berhasil Disimpan");
+window.location="form_data_admin.php";
+</script>'; 
 		 
        }  
   mysql_select_db($database_koneksi, $koneksi);
@@ -77,6 +79,31 @@ if (empty($_POST['userid']) || empty($_POST['passid'])) {
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
+<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Custom Login Form Styling</title>
+        <meta name="description" content="Custom Login Form Styling with CSS3" />
+        <meta name="keywords" content="css3, login, form, custom, input, submit, button, html5, placeholder" />
+        <meta name="author" content="Codrops" />
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+		<script src="js/modernizr.custom.63321.js"></script>
+		<!--[if lte IE 7]><style>.main{display:none;} .support-note .note-ie{display:block;}</style><![endif]-->
+		<style>	
+			@import url(http://fonts.googleapis.com/css?family=Montserrat:400,700|Handlee);
+			body {
+				background: #eedfcc url(images/bg3.jpg) no-repeat center top;
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				background-size: cover;
+			}
+			.container > header h1,
+			.container > header h2 {
+				color: #fff;
+				text-shadow: 0 1px 1px rgba(0,0,0,0.5);
+			}
+		</style>
 </head>
 
 <body>
@@ -84,21 +111,20 @@ if (empty($_POST['userid']) || empty($_POST['passid'])) {
   <input type="hidden" name="MM_insert" value="form1" />
 </form>
 <form action="<?php echo $editFormAction; ?>" method="post" name="form2" id="form2">
-  <table align="center">
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">Userid:</td>
-      <td><input type="text" name="userid" maxlength="8" value="" size="32" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">Passid:</td>
-      <td><input type="password" name="passid" maxlength="6" value="" size="32" /></td>
-    </tr>
-    <tr valign="baseline">
-      <td nowrap="nowrap" align="right">&nbsp;</td>
-      <td><input type="submit" value="masuk" />
-      <input name="Reset" type="reset" value="batal" /></td>
-    </tr>
-  </table>
+  <section class="main">
+	<center><?php echo (strlen($message) == 0) ? "" : $message ?></center>		
+		<form class="form-5 clearfix" action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1">
+				    <p>
+				        <input type="text" id="login" name="username" placeholder="Username">
+				        <input type="password" name="password" id="password" placeholder="Password"> 
+				    </p>
+				    <button type="submit" name="submit">
+				    	<i class="icon-arrow-right"></i>
+				    	<span>Sign in</span>
+						
+				    </button>     
+				
+				</form>
   <p><center>
   <a href="input_forget_password.php">Recover</a> your password if you don't remember it</center></p>
   <p>&nbsp;</p>
